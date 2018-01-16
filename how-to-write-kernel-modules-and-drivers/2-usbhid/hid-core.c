6876ace24be62e1e93421e6c28bd51b5b5f46ec5
Jason Wilkes <notarealdeveloper@gmail.com>
2018-01-16 12:31:55 -0500

    Add usbhid version 3

--- how-to-write-kernel-modules-and-drivers/2-usbhid/hid-core.c
+++ how-to-write-kernel-modules-and-drivers/2-usbhid/hid-core.c
@@ -7,7 +7,206 @@
 
 #include <linux/module.h>
 #include <linux/usb.h>
-#include "hid.h"
+
+/***************/
+/* Begin hid.h */
+/***************/
+
+#include <linux/slab.h>
+
+/* This is the collection stack. We climb up the stack to determine application and function of each field. */
+struct hid_collection {
+	unsigned type;
+	unsigned usage;
+	unsigned level;
+};
+
+struct hid_usage {
+	unsigned  hid;			/* hid usage code */
+	__u8      type;			/* input driver type */
+};
+
+struct hid_field {
+	struct hid_usage *usage;	/* usage table for this function */
+	__s32    *value;		/* last known value(s) */
+	struct hid_report *report;	/* associated report */
+	unsigned index;			/* index into report->field[] */
+	struct hid_input *hidinput;	/* associated input structure */
+};
+
+
+struct hid_report {
+	struct list_head list;
+	unsigned id;					/* id of this report */
+	unsigned type;					/* report type */
+	struct hid_field *field[256];			/* fields of the report */
+	unsigned maxfield;				/* maximum valid field index */
+	unsigned size;					/* size of the report (bits) */
+	struct hid_device *device;			/* associated device */
+};
+
+
+struct hid_report_enum {
+	unsigned numbered;
+	struct list_head report_list;
+	struct hid_report *report_id_hash[256];
+};
+
+
+struct hid_input {
+	struct list_head list;
+	struct hid_report *report;
+	struct input_dev *input;
+};
+
+enum hid_type {
+	HID_TYPE_OTHER = 0,
+	HID_TYPE_USBMOUSE,
+	HID_TYPE_USBNONE
+};
+
+struct hid_device {							/* device report descriptor */
+	__u8 *dev_rdesc;
+	unsigned dev_rsize;
+	__u8 *rdesc;
+	unsigned rsize;
+	struct hid_collection *collection;				/* List of HID collections */
+	unsigned collection_size;					/* Number of allocated hid_collections */
+	unsigned maxcollection;						/* Number of parsed collections */
+	unsigned maxapplication;					/* Number of applications */
+	__u16 bus;							/* BUS ID */
+	__u16 group;							/* Report group */
+	__u32 vendor;							/* Vendor ID */
+	__u32 product;							/* Product ID */
+	__u32 version;							/* HID version */
+	enum hid_type type;						/* device type (mouse, kbd, ...) */
+	unsigned country;						/* HID country */
+	struct hid_report_enum report_enum[3];
+	struct work_struct led_work;					/* delayed LED worker */
+
+	struct semaphore driver_lock;					/* protects the current driver, except during input */
+	struct semaphore driver_input_lock;				/* protects the current driver */
+	struct device dev;						/* device */
+	struct hid_driver *driver;
+	struct hid_ll_driver *ll_driver;
+
+	unsigned int status;						/* see STAT flags above */
+	unsigned claimed;						/* Claimed by hidinput, hiddev? */
+	unsigned quirks;						/* Various quirks the device can pull on us */
+	bool io_started;						/* Protected by driver_lock. If IO has started */
+
+	struct list_head inputs;					/* The list of inputs */
+	void *hiddev;							/* The hiddev structure */
+	void *hidraw;
+	int minor;							/* Hiddev minor number */
+
+	int open;							/* is the device open by anyone? */
+	char name[128];							/* Device name */
+	char phys[64];
+	void *driver_data;
+
+	/* temporary hid_ff handling (until moved to the drivers) */
+	int (*ff_init)(struct hid_device *);
+
+	/* hiddev event handler */
+	int (*hiddev_connect)(struct hid_device *, unsigned int);
+	void (*hiddev_disconnect)(struct hid_device *);
+	void (*hiddev_hid_event) (struct hid_device *, struct hid_field *field, struct hid_usage *, __s32);
+	void (*hiddev_report_event) (struct hid_device *, struct hid_report *);
+
+	/* debugging support via debugfs */
+	unsigned short debug;
+	struct dentry *debug_dir;
+	struct dentry *debug_rdesc;
+	struct dentry *debug_events;
+	struct list_head debug_list;
+	spinlock_t  debug_list_lock;
+	wait_queue_head_t debug_wait;
+};
+
+struct hid_class_descriptor {
+	__u8  bDescriptorType;
+	__le16 wDescriptorLength;
+} __attribute__ ((packed));
+
+struct hid_descriptor {
+	__u8  bLength;
+	__u8  bDescriptorType;
+	__le16 bcdHID;
+	__u8  bCountryCode;
+	__u8  bNumDescriptors;
+
+	struct hid_class_descriptor desc[1];
+} __attribute__ ((packed));
+
+
+struct hid_report_id {
+	__u32 report_type;
+};
+struct hid_usage_id {
+	__u32 usage_hid;
+	__u32 usage_type;
+	__u32 usage_code;
+};
+
+
+struct hid_driver {
+	char *name;
+	const struct hid_device_id *id_table;
+	struct list_head dyn_list;
+	spinlock_t dyn_lock;
+	int (*probe)(struct hid_device *dev, const struct hid_device_id *id);
+	void (*remove)(struct hid_device *dev);
+	const struct hid_report_id *report_table;
+	int (*raw_event)(struct hid_device *hdev, struct hid_report *report, u8 *data, int size);
+	const struct hid_usage_id *usage_table;
+	int (*event)(struct hid_device *hdev, struct hid_field *field, struct hid_usage *usage, __s32 value);
+	void (*report)(struct hid_device *hdev, struct hid_report *report);
+	__u8 *(*report_fixup)(struct hid_device *hdev, __u8 *buf, unsigned int *size);
+	int (*input_mapping)(struct hid_device *hdev, struct hid_input *hidinput, struct hid_field *field, struct hid_usage *usage, 
+		unsigned long **bit, int *max);			
+	int (*input_mapped)(struct hid_device *hdev, struct hid_input *hidinput, struct hid_field *field, struct hid_usage *usage, 
+		unsigned long **bit, int *max);
+	void (*input_configured)(struct hid_device *hdev,struct hid_input *hidinput);
+	void (*feature_mapping)(struct hid_device *hdev,struct hid_field *field,struct hid_usage *usage);
+	struct device_driver driver;
+};
+
+
+struct hid_ll_driver {
+	int  (*start)(struct hid_device *hdev);
+	void (*stop)(struct hid_device *hdev);
+	int  (*open)(struct hid_device *hdev);
+	void (*close)(struct hid_device *hdev);
+	int  (*power)(struct hid_device *hdev, int level);
+	int  (*parse)(struct hid_device *hdev);
+	void (*request)(struct hid_device *hdev, struct hid_report *report, int reqtype);
+	int  (*wait)(struct hid_device *hdev);
+	int  (*raw_request) (struct hid_device *hdev, unsigned char reportnum, __u8 *buf, size_t len, unsigned char rtype, int reqtype);
+	int  (*idle)(struct hid_device *hdev, int report, int idle, int reqtype);
+};
+
+
+/* HID core API */
+extern int hid_debug;
+extern bool hid_ignore(struct hid_device *);
+extern int hid_add_device(struct hid_device *);
+extern void hid_destroy_device(struct hid_device *);
+extern int __must_check __hid_register_driver(struct hid_driver *, struct module *, const char *mod_name);
+extern void hid_unregister_driver(struct hid_driver *);
+
+
+#define module_hid_driver(__hid_driver) \
+	module_driver(__hid_driver, hid_register_driver, hid_unregister_driver)
+
+const struct hid_device_id *hid_match_id(struct hid_device *hdev, const struct hid_device_id *id);
+s32 hid_snto32(__u32 value, unsigned n);
+
+
+/****************/
+/* End of hid.h */
+/****************/
+
 
 
 int hid_input_report(struct hid_device *, int type, u8 *, int, int);
@@ -62,7 +261,6 @@ static void hid_ctrl(struct urb *urb)
 int usbhid_open(struct hid_device *hid)
 {
 	struct usbhid_device *usbhid = hid->driver_data;
-	hid->open++;
 	usb_submit_urb(usbhid->urbin, GFP_ATOMIC);
 	clear_bit(3, &usbhid->iofl);
 	usb_autopm_put_interface(usbhid->intf);
@@ -118,7 +316,9 @@ static int usbhid_parse(struct hid_device *hid)
 
 	usb_get_extra_descriptor(interface, (USB_TYPE_CLASS | 0x01), &hdesc);
 
+	printk("Before: rsize = %d\n", rsize);
 	rsize = le16_to_cpu(hdesc->desc[0].wDescriptorLength);
+	printk("After: rsize = %d\n", rsize);
 	rdesc = kmalloc(rsize, GFP_KERNEL);
 
 	usb_control_msg(dev, usb_sndctrlpipe(dev, 0), 0x0A, USB_TYPE_CLASS | USB_RECIP_INTERFACE, 
@@ -145,7 +345,7 @@ static int usbhid_start(struct hid_device *hid)
 	int interval;
 	unsigned int insize = 0;
 
-	// clear_bit(7, &usbhid->iofl);
+	clear_bit(7, &usbhid->iofl);
 	usbhid->bufsize = 64;
 	hid_find_max_report(hid, 0, &insize);
 	hid_alloc_buffers(dev, hid);
@@ -219,7 +419,6 @@ static int usbhid_probe(struct usb_interface *intf, const struct usb_device_id *
 	hid->driver_data = usbhid;
 	usbhid->hid = hid;
 	usbhid->intf = intf;
-	usbhid->ifnum = interface->desc.bInterfaceNumber;
 
 	hid_add_device(hid);
 
