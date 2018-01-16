ec071708f0bd3154023a42e41faf0ead7f16edc6
Jason Wilkes <notarealdeveloper@gmail.com>
2018-01-16 12:31:55 -0500

    Add usbhid version 6

--- how-to-write-kernel-modules-and-drivers/2-usbhid/hid-core.c
+++ how-to-write-kernel-modules-and-drivers/2-usbhid/hid-core.c
@@ -9,6 +9,8 @@
 #include <linux/usb.h>
 #include <linux/slab.h>
 
+#define enter() printk(KERN_DEBUG "[+] %s: entering\n", __func__)
+#define leave() printk(KERN_DEBUG "[-] %s: leaving\n", __func__)
 /***************/
 /* Begin hid.h */
 /***************/
@@ -25,8 +27,8 @@ struct hid_report {
 	struct list_head list;
 	unsigned id;					/* id of this report */
 	unsigned type;					/* report type */
-	void *fuckit[256];   				/* fields of the report */
-	unsigned maxfield;				/* maximum valid field index */
+	void *fuckit_0[256];
+	unsigned fuckit_1;
 	unsigned size;					/* size of the report (bits) */
 	struct hid_device *device;			/* associated device */
 };
@@ -35,7 +37,8 @@ struct hid_report {
 struct hid_report_enum {
 	unsigned numbered;
 	struct list_head report_list;
-	struct hid_report *report_id_hash[256];
+	//struct hid_report *report_id_hash[256];
+	void *fuckit_0[256];
 };
 
 
@@ -48,59 +51,29 @@ struct hid_input {
 /* Something in usbcore is using this. Can't make normal assumptions
  * Update: The weird behavior was because the order of members in this structure matters! 
  * Something in usbcore or elsewhere must be *parsing* this shit! */
-struct hid_device {							/* device report descriptor */
-	u8 	*fuckit_0;
-	unsigned fuckit_1;
+struct hid_device {					/* device report descriptor */
+	char 	fuckit_1[16];
 	u8 	*rdesc;
 	unsigned rsize;
-	char fuckit_2[24];
-	u16 bus;							/* BUS ID */
-	char fuckit_3[22];
-	struct hid_report_enum report_enum[3];
-	char fuckit_4[32];
-
-	struct semaphore driver_lock;					/* protects the current driver, except during input */
-	struct semaphore driver_input_lock;				/* protects the current driver */
-	struct device dev;						/* device */
-	struct hid_driver *driver;
-	struct hid_ll_driver *ll_driver;
-
-	unsigned int status;						/* see STAT flags above */
-	unsigned claimed;						/* Claimed by hidinput, hiddev? */
-	unsigned quirks;						/* Various quirks the device can pull on us */
-	bool io_started;						/* Protected by driver_lock. If IO has started */
-
-	struct list_head inputs;					/* The list of inputs */
-	void *hiddev;							/* The hiddev structure */
-	void *hidraw;
-	int minor;							/* Hiddev minor number */
-
-	int open;							/* is the device open by anyone? */
-	char name[128];							/* Device name */
-	char phys[64];
-	void *driver_data;
+	char 	fuckit_2[24];
+	u16 	bus;					/* BUS ID */
+	char 	fuckit_3[22];
+	struct 	hid_report_enum report_enum[3];
+	char 	fuckit_4[80];
+	struct 	device dev;				/* device */
+	char 	fuckit_5[8];
+	struct 	hid_ll_driver *ll_driver;
+	char 	fuckit_6[56];
+	char 	name[128];				/* Device name */
+	char 	phys[64];
+	void 	*driver_data;
 };
 
 
 struct hid_descriptor {
-	u8  bLength;
-	u8  bDescriptorType;
-	u16 bcdHID;
-	u8  bCountryCode;
-	u8  bNumDescriptors;
-	u8  wtf_usb;
-	u16 wDescriptorLength;
-} __attribute__ ((packed));
-
-
-struct hid_driver {
-	char *name;
-	const struct hid_device_id *id_table;
-	int (*probe)(struct hid_device *dev, const struct hid_device_id *id);
-	void (*remove)(struct hid_device *dev);
-	void (*report)(struct hid_device *hdev, struct hid_report *report);
-	struct device_driver driver;
-};
+	u8  fuckit[7];
+	u16 len;
+} __attribute__((packed));
 
 
 struct hid_ll_driver {
@@ -138,8 +111,6 @@ struct usbhid_device {
 	struct usb_interface *intf;                                     /* USB interface */
 	int ifnum;                                                      /* USB interface number */
 
-	unsigned int bufsize;                                           /* URB buffer size */
-
 	struct urb *urbin;                                              /* Input URB */
 	char *inbuf;                                                    /* Input buffer */
 	dma_addr_t inbuf_dma;                                           /* Input buffer dma */
@@ -147,13 +118,11 @@ struct usbhid_device {
 	struct urb *urbctrl;                                            /* Control URB */
 	struct usb_ctrlrequest *cr;                                     /* Control request struct */
 	char *ctrlbuf;                                                  /* Control buffer */
-
-	unsigned long iofl;                                             /* I/O flags (CTRL_RUNNING, OUT_RUNNING) */
 };
 
-#define	hid_to_usb_dev(hid_dev) \
-	container_of(hid_dev->dev.parent->parent, struct usb_device, dev)
+#define usbhid_bufsize	64
 
+#define	hid_to_usb_dev(hid_dev) 	container_of(hid_dev->dev.parent->parent, struct usb_device, dev)
 
 /* Input interrupt completion handler. */
 static void hid_irq_in(struct urb *urb)
@@ -165,49 +134,20 @@ static void hid_irq_in(struct urb *urb)
 int usbhid_open(struct hid_device *hid)
 {
 	struct usbhid_device *usbhid = hid->driver_data;
+	enter();
 	usb_submit_urb(usbhid->urbin, GFP_ATOMIC);
-	clear_bit(3, &usbhid->iofl);
 	usb_autopm_put_interface(usbhid->intf);
-
-	// printk("NIG NOG! %lu\n", sizeof(struct work_struct));
+	leave();
 	return 0;
 }
 
 void usbhid_close(struct hid_device *hid)
 {
 	struct usbhid_device *usbhid = hid->driver_data;
+	enter();
 	usb_kill_urb(usbhid->urbin);
 	usbhid->intf->needs_remote_wakeup = 0;
-}
-
-
-/* Traverse the supplied list of reports and find the longest */
-static void hid_find_max_report(struct hid_device *hid, unsigned int type, unsigned int *max)
-{
-	struct hid_report *report;
-	unsigned int size;
-	list_for_each_entry(report, &hid->report_enum[type].report_list, list) {
-		size = ((report->size - 1) >> 3) + 1 + hid->report_enum[type].numbered;
-		if (*max < size)
-			*max = size;
-	}
-}
-
-static int hid_alloc_buffers(struct usb_device *dev, struct hid_device *hid)
-{
-	struct usbhid_device *usbhid = hid->driver_data;
-	usbhid->inbuf = usb_alloc_coherent(dev, usbhid->bufsize, GFP_KERNEL, &usbhid->inbuf_dma);
-	usbhid->cr = kzalloc(sizeof(*usbhid->cr), GFP_KERNEL);
-
-	return 0;
-}
-
-
-static void hid_free_buffers(struct usb_device *dev, struct hid_device *hid)
-{
-	struct usbhid_device *usbhid = hid->driver_data;
-	usb_free_coherent(dev, usbhid->bufsize, usbhid->inbuf, usbhid->inbuf_dma);
-	kfree(usbhid->cr);
+	leave();
 }
 
 static int usbhid_parse(struct hid_device *hid)
@@ -218,53 +158,67 @@ static int usbhid_parse(struct hid_device *hid)
 	struct hid_descriptor *hdesc;
 	unsigned int rsize = 0;
 	char *rdesc;
-	int ifnum = interface->desc.bInterfaceNumber;
+	int ifnum;
 
+	enter();
+	ifnum = interface->desc.bInterfaceNumber;
 	usb_get_extra_descriptor(interface, (USB_TYPE_CLASS | 0x01), &hdesc);
+	rsize = hdesc->len;
+	rdesc = kzalloc(rsize, GFP_KERNEL);
 
-	printk("Before: rsize = %d\n", rsize);
-	rsize = le16_to_cpu(hdesc->wDescriptorLength);
-	printk("After: rsize = %d\n", rsize);
-	rdesc = kmalloc(rsize, GFP_KERNEL);
-
-	usb_control_msg(dev, usb_sndctrlpipe(dev, 0), 0x0A, USB_TYPE_CLASS | USB_RECIP_INTERFACE, 
-		0, ifnum, NULL, 0, USB_CTRL_SET_TIMEOUT);
-
-	memset(rdesc, 0, rsize);
+	/* Final argument == timeout == 5000 msec */
+	usb_control_msg(dev, usb_sndctrlpipe(dev, 0), 0x0A, USB_TYPE_CLASS | USB_RECIP_INTERFACE, 0, ifnum, NULL, 0, 5000);
 	usb_control_msg(dev, usb_rcvctrlpipe(dev, 0), USB_REQ_GET_DESCRIPTOR, USB_RECIP_INTERFACE | USB_DIR_IN, 
-		((USB_TYPE_CLASS | 0x02) << 8), ifnum, rdesc, rsize, USB_CTRL_GET_TIMEOUT);
+		((USB_TYPE_CLASS | 0x02) << 8), ifnum, rdesc, rsize, 5000);
 
 	hid_parse_report(hid, rdesc, rsize);
 	kfree(rdesc);
-
+	leave();
 	return 0;
 }
 
 static int usbhid_start(struct hid_device *hid)
 {
-	struct usb_interface *intf = to_usb_interface(hid->dev.parent);
-	struct usb_host_interface *interface = intf->cur_altsetting;
-	struct usb_device *dev = interface_to_usbdev(intf);
-	struct usbhid_device *usbhid = hid->driver_data;
-	struct usb_endpoint_descriptor *endpoint;
+	struct usb_interface 		*intf 		= to_usb_interface(hid->dev.parent);
+	struct usb_host_interface 	*interface 	= intf->cur_altsetting;
+	struct usb_device 		*dev 		= interface_to_usbdev(intf);
+	struct usbhid_device 		*usbhid 	= hid->driver_data;
+	struct usb_endpoint_descriptor 	*endpoint;
 	int pipe;
 	int interval;
-	unsigned int insize = 0;
 
-	clear_bit(7, &usbhid->iofl);
-	usbhid->bufsize = 64;
-	hid_find_max_report(hid, 0, &insize);
-	hid_alloc_buffers(dev, hid);
+	struct hid_report *report;
+	unsigned int size;
+	unsigned int maxsize = 0;
+
+	enter();
+	//printk("sizeof(struct list_head) == %lu\n", sizeof(struct list_head));
+	//printk("&(hid->fuckit_6[12]) == %p\n", &(hid->fuckit_6[12]));
+	//printk("&(hid->fuckit_0) == %p\n", &(hid->fuckit_0));
+	//printk("&(hid->fuckit_1) == %p\n", &(hid->fuckit_1));
+	//printk("&(hid->fuckit_7[0]) == %p\n", &(hid->fuckit_7[0]));
+	// printk("&(hid->inputs)       == %p\n", &(hid->inputs));
+	/* Was hid_find_max_report */
+	list_for_each_entry(report, &hid->report_enum[0].report_list, list) {
+		size = ((report->size - 1) >> 3) + 1 + hid->report_enum[0].numbered;
+		if (maxsize < size)
+			maxsize = size;
+	}
+
+	/* Was hid_alloc_buffers */
+	usbhid->inbuf = usb_alloc_coherent(dev, usbhid_bufsize, GFP_KERNEL, &usbhid->inbuf_dma);
+	usbhid->cr = kzalloc(sizeof(*usbhid->cr), GFP_KERNEL);
+
+
 	endpoint = &interface->endpoint[0].desc;
 	interval = endpoint->bInterval;
 	usbhid->urbin = usb_alloc_urb(0, GFP_KERNEL);
 	pipe = usb_rcvintpipe(dev, endpoint->bEndpointAddress);
 
-	usb_fill_int_urb(usbhid->urbin, dev, pipe, usbhid->inbuf, insize, hid_irq_in, hid, interval);
+	usb_fill_int_urb(usbhid->urbin, dev, pipe, usbhid->inbuf, maxsize, hid_irq_in, hid, interval);
 
 	usbhid->urbctrl = usb_alloc_urb(0, GFP_KERNEL);
-	set_bit(8, &usbhid->iofl);
-
+	leave();
 	return 0;
 }
 
@@ -272,13 +226,14 @@ static void usbhid_stop(struct hid_device *hid)
 {
 	struct usbhid_device *usbhid = hid->driver_data;
 
-	clear_bit(8, &usbhid->iofl);
-	set_bit(7, &usbhid->iofl);
+	enter();
 	usb_kill_urb(usbhid->urbin);
 	usb_kill_urb(usbhid->urbctrl);
 	usb_free_urb(usbhid->urbin);
 	usb_free_urb(usbhid->urbctrl);
-	hid_free_buffers(hid_to_usb_dev(hid), hid); // Probably causes leaks
+	usb_free_coherent(hid_to_usb_dev(hid), usbhid_bufsize, usbhid->inbuf, usbhid->inbuf_dma);
+	kfree(usbhid->cr);
+	leave();
 }
 
 static int usbhid_raw_request(struct hid_device *hid, u8 reportnum, __u8 *buf, size_t len, u8 rtype, int reqtype)
@@ -305,6 +260,7 @@ static int usbhid_probe(struct usb_interface *intf, const struct usb_device_id *
 	struct usbhid_device *usbhid;
 	struct hid_device *hid;
 
+	enter();
 	usb_endpoint_is_int_in(&interface->endpoint[0].desc);
 
 	hid = hid_allocate_device();
@@ -322,7 +278,7 @@ static int usbhid_probe(struct usb_interface *intf, const struct usb_device_id *
 	usbhid->intf = intf;
 
 	hid_add_device(hid);
-
+	leave();
 	return 0;
 }
 
@@ -330,10 +286,11 @@ static void usbhid_disconnect(struct usb_interface *intf)
 {
 	struct hid_device *hid = usb_get_intfdata(intf);
 	struct usbhid_device *usbhid;
-
+	enter();
 	usbhid = hid->driver_data;
 	hid_destroy_device(hid);
 	kfree(usbhid);
+	leave();
 }
 
 
@@ -349,20 +306,23 @@ static struct usb_driver hid_driver = {
 	.id_table =	hid_usb_ids,
 };
 
-static int __init hid_init(void)
+static int __init usbhid_init(void)
 {
+	enter();
 	usb_register(&hid_driver);
-	printk(KERN_INFO KBUILD_MODNAME ": " "USB HID core driver" "\n");
+	leave();
 	return 0;
 }
 
-static void __exit hid_exit(void)
+static void __exit usbhid_exit(void)
 {
+	enter();
 	usb_deregister(&hid_driver);
+	leave();
 }
 
-module_init(hid_init);
-module_exit(hid_exit);
+module_init(usbhid_init);
+module_exit(usbhid_exit);
 
 MODULE_DEVICE_TABLE (usb, hid_usb_ids);
 MODULE_AUTHOR("Blayson! Muahaha!");
